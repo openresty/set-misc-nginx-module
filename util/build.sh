b3fb207b4d657d0a066e1395d8cb364960f7eb1e
agentzh (章亦春) <agentzh@gmail.com>
2010-07-07 16:48:40 +0800

    fixed build.sh a bit.

--- util/build.sh
+++ util/build.sh
@@ -2,13 +2,14 @@
 
 # this file is mostly meant to be used by the author himself.
 
-rm ~/work/nginx-0.8.41/objs/addon/ndk/ndk.o ~/work/nginx-0.8.41/objs/addon/ndk-nginx-module/ndk.o
-
 root=`pwd`
 home=~
 cd ~/work
 version=$1
 opts=$2
+
+rm -f ~/work/nginx-$version/objs/addon/ndk/ndk.o \
+    ~/work/nginx-$version/objs/addon/ndk-nginx-module/ndk.o
 
 if [ ! -s "nginx-$version.tar.gz" ]; then
     wget "http://sysoev.ru/nginx/nginx-$version.tar.gz" -O nginx-$version.tar.gz || exit 1
@@ -31,10 +32,10 @@ if [[ "$BUILD_CLEAN" = 1 || ! -f Makefile || "$root/config" -nt Makefile || "$ro
           --add-module=$root/../echo-nginx-module \
           --add-module=$root/../ndk-nginx-module \
           --add-module=$root $opts \
-          --with-debug
+          --with-debug \
+      || exit 1
           #--add-module=$home/work/ndk \
   #--without-http_ssi_module  # we cannot disable ssi because echo_location_async depends on it (i dunno why?!)
-
 fi
 if [ -f /opt/nginx/sbin/nginx ]; then
     rm -f /opt/nginx/sbin/nginx
@@ -42,6 +43,5 @@ fi
 if [ -f /opt/nginx/logs/nginx.pid ]; then
     kill `cat /opt/nginx/logs/nginx.pid`
 fi
-make -j3
-make install
+make -j3 && make install
 
