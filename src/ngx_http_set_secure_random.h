273dce3dbc2551dc3283b1bfe5ab8b04512cab75
Chris J. Kiick <ckiick@atlassian.com>
2015-11-19 14:45:54 -0600

    Add set_secure_random_hex

--- src/ngx_http_set_secure_random.h
+++ src/ngx_http_set_secure_random.h
@@ -11,5 +11,8 @@ ngx_int_t ngx_http_set_misc_set_secure_random_alphanum(ngx_http_request_t *r,
 ngx_int_t ngx_http_set_misc_set_secure_random_lcalpha(ngx_http_request_t *r,
         ngx_str_t *res, ngx_http_variable_value_t *v);
 
+ngx_int_t ngx_http_set_misc_set_secure_random_hex(ngx_http_request_t *r,
+        ngx_str_t *res, ngx_http_variable_value_t *v);
+
 #endif /* NGX_SET_SECURE_RANDOM_H */
 
