45c28ada86394189f45678ab2647403c34a87f72
erankor <eran.kornblau@kaltura.com>
2021-09-03 17:50:41 +0300

    feature: added new directive set_hmac_sha256. (#57)

--- src/ngx_http_set_misc_module.c
+++ src/ngx_http_set_misc_module.c
@@ -80,6 +80,14 @@ static ndk_set_var_t  ngx_http_set_misc_set_hmac_sha1_filter = {
     2,
     NULL
 };
+
+
+static ndk_set_var_t  ngx_http_set_misc_set_hmac_sha256_filter = {
+    NDK_SET_VAR_MULTI_VALUE,
+    (void *) ngx_http_set_misc_set_hmac_sha256,
+    2,
+    NULL
+};
 #endif
 
 
@@ -248,6 +256,14 @@ static ngx_command_t  ngx_http_set_misc_commands[] = {
         0,
         0,
         &ngx_http_set_misc_set_hmac_sha1_filter
+    },
+    {   ngx_string ("set_hmac_sha256"),
+        NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_SIF_CONF
+            |NGX_HTTP_LOC_CONF|NGX_HTTP_LIF_CONF|NGX_CONF_TAKE3,
+        ndk_set_var_multi_value,
+        0,
+        0,
+        &ngx_http_set_misc_set_hmac_sha256_filter
     },
 #endif
 #ifndef NGX_HTTP_SET_HASH
